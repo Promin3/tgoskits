326880fe5b8305634951e908933e0aed46968d0b
Ivans-11 <w-yf22@mails.tsinghua.edu.cn>
2026-04-27 23:46:07 +0800

    refactor(axvisor): drop local riscv64 guest test platform

--- test-suit/axvisor/common/guestlib/src/lib.rs
+++ test-suit/axvisor/common/guestlib/src/lib.rs
@@ -1,7 +1,5 @@
 #![cfg_attr(feature = "ax-std", no_std)]
 
-#[cfg(all(target_arch = "riscv64", feature = "riscv64-qemu-virt-test"))]
-extern crate ax_plat_riscv64_qemu_virt_test;
 #[cfg(feature = "ax-std")]
 extern crate ax_std as std;
 
